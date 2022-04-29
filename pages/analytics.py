77ff3417b00adffdf540b7c12f1563d3bf174f43
edjanga <emmanuel.djanga@live.be>
2022-04-29 10:50:07 +0100

    Fix bug in analytics.py

--- pages/analytics.py
+++ pages/analytics.py
@@ -19,14 +19,11 @@ layout = html.Div([html.H1('Crypto Dashboard',className='header',id='page'),\
                                            value='box', inline=True, inputStyle={'margin': '10px'})]),
                   html.Div(children=[dcc.Graph(id='page_content_analytics', figure={})])])
 
-#dcc.Interval(id='interval-component',interval=1*1000,n_intervals=0),\
-
 @dash_app.callback(
     Output(component_id='page_content_analytics',component_property='figure'),
     [Input(component_id='dropdown',component_property='value'),\
      Input(component_id='box_plot_type',component_property='value')]
-)#,\
-#     Input(component_id='interval-component', component_property='n_intervals')
+)
 def generate_plot(ticker,box_plot_type):
     if ticker != '':
         if (isinstance(ticker,list))&(len(ticker)>1):
@@ -43,10 +40,9 @@ def generate_plot(ticker,box_plot_type):
             query = 'SELECT * FROM dummy_data WHERE ticker = "%s" AND indicator = "close"' %ticker[0]
         df = pd.read_sql(sql=query,\
                          con=data_dummy_obj.dummy_conn_obj,index_col='date').drop(['index','indicator'],axis=1)
+
         df = pd.pivot_table(df,values='price',index='date',columns='ticker').pct_change().reset_index(False)
-        df.loc[:,'returns'] = df.loc[:,'price'].pct_change()
         df = pd.melt(df,id_vars='date',value_name='returns').dropna()
-        filter_s = df['ticker'].isin(ticker)
         title = 'Return distribution'
         fig = px.histogram(df,y='returns',color='ticker',marginal=box_plot_type,title =title)
         return fig
